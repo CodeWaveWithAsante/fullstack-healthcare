e288ddacc4a99bca4435d002cdea2e62498eae5c
Codewave <132752907+CodeWaveWithAsante@users.noreply.github.com>
2024-12-30 14:31:58 +0530

    Add files via upload

--- app/(protected)/patient/registration/page.tsx
+++ app/(protected)/patient/registration/page.tsx
@@ -1,14 +1,18 @@
 import { NewPatient } from "@/components/new-patient";
+import { getPatientById } from "@/utils/services/patient";
 import { auth } from "@clerk/nextjs/server";
 import React from "react";
 
 const Registration = async () => {
   const { userId } = await auth();
-  const data = null;
-  // const {data} = await getPatientDataById(userId)
+
+  const { data } = await getPatientById(userId!);
+
   return (
-    <div className="py-6 px-3 min-h-full flex justify-center">
-      <NewPatient data={data!} type={!data ? "create" : "update"} />
+    <div className="w-full h-full flex justify-center">
+      <div className="max-w-6xl w-full relative pb-10">
+        <NewPatient data={data!} type={!data ? "create" : "update"} />
+      </div>
     </div>
   );
 };
